e2e426482d1f022eec5df969763a56e04270449c
ehsan74 <ehsanmanesh@unboundgamestudio.com>
2021-08-12 21:52:29 +0430

    Using EditorApplication.delayCall instead of coroutines package.

--- MagicLayout_Project/Assets/MagicLayout/Editor/MagicLayout.cs
+++ MagicLayout_Project/Assets/MagicLayout/Editor/MagicLayout.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 using UnityEditor;
 using System;
 using System.Reflection;
-using Unity.EditorCoroutines.Editor;
-using System.Collections;
 using Object = UnityEngine.Object;
 
 namespace R4hulCorleone
 {
-    [InitializeOnLoad] 
+    [InitializeOnLoad]
     public class MagicLayout
     {
         static MagicLayout()
@@ -17,26 +15,32 @@ namespace R4hulCorleone
             EnableResizableOnViewChanged();
         }
 
-        #region OnInitialize
         private static void EnableResizeableAfterInitialize()
         {
-            EditorCoroutineUtility.StartCoroutineOwnerless(MakeResizeableAfterInitialize_Coroutine());
+            EditorApplication.delayCall += () =>
+            {
+                EditorApplication.delayCall += MakeAvailableWindowsResizable;
+            };
         }
 
-        private static IEnumerator MakeResizeableAfterInitialize_Coroutine()
+        private static void MakeAvailableWindowsResizable()
         {
-            yield return null;
-            MakeAvailableWindowsResizable();
-        }
-        #endregion
+            var windows = Resources.FindObjectsOfTypeAll<EditorWindow>();
+            foreach (var window in windows)
+            {
+                if (window.titleContent.text == "Scene" || window.titleContent.text == "Game")
+                    continue;
 
-        #region OnViewChanged
+                window.minSize = Vector2.zero;
+            }
+        }
+
         private static void EnableResizableOnViewChanged()
         {
-            HookUpViewChanged();
+            HookUpOnViewChanged();
         }
 
-        private static void HookUpViewChanged()
+        private static void HookUpOnViewChanged()
         {
             var hostViewType = typeof(Editor).Assembly.GetType("UnityEditor.HostView");
             var viewChangedEvent = hostViewType.GetEvent("actualViewChanged", BindingFlags.NonPublic | BindingFlags.Static);
@@ -57,27 +61,7 @@ namespace R4hulCorleone
 
         private static void OnViewChanged(Object hostView)
         {
-            EditorCoroutineUtility.StartCoroutineOwnerless(MakeFocusedWindowResizable_Coroutine());
-        }
-
-        private static IEnumerator MakeFocusedWindowResizable_Coroutine()
-        {
-            yield return null;
-            MakeAvailableWindowsResizable();
-        }
-        #endregion
-
-        private static void MakeAvailableWindowsResizable()
-        {
-            var windows = Resources.FindObjectsOfTypeAll<EditorWindow>();
-            foreach (var window in windows)
-            {
-                if (window.titleContent.text == "Scene" || window.titleContent.text == "Game")
-                    continue;
-
-                window.minSize = Vector2.zero;
-            }
+            EditorApplication.delayCall += MakeAvailableWindowsResizable;
         }
     }
 }
-
